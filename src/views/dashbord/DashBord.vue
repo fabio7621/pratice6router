bd810e693b4ea9a585cdf94b69938afbf2ca2a8d
fabio7621 <d6200121a@gmail.com>
2024-02-11 14:30:36 +0800

    進入後台驗證ＯＫ

--- src/views/dashbord/DashBord.vue
+++ src/views/dashbord/DashBord.vue
@@ -44,3 +44,24 @@ nav a:first-of-type {
 	}
 }
 </style>
+<script>
+export default {
+	methods: {
+		checklogin() {
+			const api = `${import.meta.env.VITE_API}api/user/check`;
+			this.$http
+				.post(api)
+				.then((res) => {
+					console.log(res.data.success);
+				})
+				.catch((err) => {
+					alert(err.response.data.message);
+					this.$router.push("/login");
+				});
+		},
+	},
+	mounted() {
+		this.checklogin();
+	},
+};
+</script>
