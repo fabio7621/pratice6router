af1c98ec865aaaf6d335ed6db8d232a1434edc2b
fabio7621 <d6200121a@gmail.com>
2024-02-15 09:06:25 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "pratice6router/",
+  base: "pratice6router",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
